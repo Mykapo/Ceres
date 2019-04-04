3185255606bfe7271140ee4336984aa2bcdf1919
Molika Thai <molika.thai@hetic.net>
2019-04-04 17:14:04 +0200

    missions - add tag and mission title

--- gaiaApp/UIComponents/Missions List/Mission Cells/LargeMissionCell.swift
+++ gaiaApp/UIComponents/Missions List/Mission Cells/LargeMissionCell.swift
@@ -38,11 +38,16 @@ class LargeMissionCell: MissionCell {
     
     override func setupViews() {
         largeMissionImg.translatesAutoresizingMaskIntoConstraints = false
+        missionTag.translatesAutoresizingMaskIntoConstraints = false
+        missionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         addSubview(largeMissionImg)
         addSubview(missionTag)
         addSubview(missionLabel)
         
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[v0]-20-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": largeMissionImg]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": largeMissionImg]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-24-[v0]-24-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": largeMissionImg]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-24-[tag]-24-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["tag": missionTag]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-24-[label]-24-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["label": missionLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0][tag][label]-25-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": largeMissionImg, "tag": missionTag, "label": missionLabel]))
     }
 }
