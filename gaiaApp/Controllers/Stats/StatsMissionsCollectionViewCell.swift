f8764cabb012ec1e08590276dddc65c86b69ee7d
Molika Thai <molika.thai@hetic.net>
2019-06-04 15:37:00 +0200

    stats - add missions collection view

---
+++ gaiaApp/Controllers/Stats/StatsMissionsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  StatsMissionsCollectionViewCell.swift
+//  gaiaApp
+//
+//  Created by Molika THAI on 04/06/2019.
+//  Copyright © 2019 hetic. All rights reserved.
+//
+
+import UIKit
+
+class StatsMissionsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var missionsNb: UILabel!
+    @IBOutlet weak var missionsType: UILabel!
+    
+}
